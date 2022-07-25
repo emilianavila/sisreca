284d92ae2ea88624905b1939fe033642de4e1ab7
emilianavila <vilaemiliana@gmail.com>
2022-07-25 16:33:24 -0400

    lunes 25

--- sisreca/resources/views/auth/forgot-password.blade.php
+++ sisreca/resources/views/auth/forgot-password.blade.php
@@ -5,7 +5,7 @@
         </x-slot>
 
         <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            {{ __('¿Olvidaste tu contraseña? No hay problema. Simplemente escribe tu dirección de correo electrónico y te enviaremos un enlace de restablecimiento de contraseña que te permitirá elegir una nueva.') }}
         </div>
 
         @if (session('status'))
@@ -26,7 +26,7 @@
 
             <div class="flex items-center justify-end mt-4">
                 <x-jet-button>
-                    {{ __('Email Password Reset Link') }}
+                    {{ __('Enlace para restablecer contraseña') }}
                 </x-jet-button>
             </div>
         </form>
